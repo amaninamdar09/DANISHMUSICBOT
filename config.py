eb12f664db50b79af76d1826e6bfc3d3ab0db9e5
Aman Inamdar <79567211+amaninamdar09@users.noreply.github.com>
2022-02-28 17:00:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,16 +7,65 @@ load_dotenv()
 # VARS
 
 get_queue = {}
-STRING_SESSION1 = AQAJ8gU594D7BR3jasckDWMKQsdDLWNls3rS_Bu1I4HmYQYCfMFIuEnYE2GNA9rFS625Mj_VLcSSIUpjliC_8_ehwSGHCL5yAV4JMHVFctw0muv-SxNujY8ugO-NmKFwXUDDpF2fxIYGlWOauV3Yg801KTxmN4EKz8UVeoXkZBcZLHuFfIMHnBBRerLbHpksNyfQej0p7cd_sECChe-UhPtgyKq9TRkLbmG4Oxx-qWDWH-QkUPZES5Q08VBG_4SkYp7hsIcuLOzQR_1SCi6cyb4exjH5wkIHqIXqfgODkHLNudJljIl153jy2ZRHWHL1nMCoru4wesFAkI8bX1XsuG_8AAAAATf_Kb0A 
-API_ID = 11977266
-API_HASH = a128248e7223b86e6d1c417e8f355704
-BOT_TOKEN = 5157142486:AAHuWCq24yXRIq6MBPaT-B4nYyoEU_ybD84
-SUDO_USERS = 1968830241 
-MONGO_DB_URI = mongodb+srv://kvaiyukki09:kvaiyukki09@cluster0.qvhkz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-LOG_GROUP_ID = -1001510782734
-MUSIC_BOT_NAME = 𝐃𝐀𝐍𝐈𝐒𝐇 𝐌𝐔𝐒𝐈𝐂 𝐁𝐎𝐓 
-OWNER_ID = 1968830241
-DURATION_LIMIT = 180
-SUPPORT_CHANNEL = https://t.me/DANISH_BABA 
-SUPPORT_GROUP = https://t.me/WEFRIENDSCIRCLE
-ASSISTANT_PREFIX = .
+BOT_TOKEN = getenv("BOT_TOKEN")
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH")
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "10"))
+ASSISTANT_PREFIX = list(getenv("ASSISTANT_PREFIX", ".").split())
+MONGO_DB_URI = getenv("MONGO_DB_URI")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
+HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+
+UPSTREAM_REPO = "https://github.com/amaninamdar09/DANISHMUSICBOT"
+UPSTREAM_BRANCH = "main"
+
+SUPPORT_CHANNEL = "https://t.me/DANISH_BABA"
+SUPPORT_GROUP = "https://t.me/WEFRIENDSCIRCLE"
+
+THUMBNAIL = getenv("THUMB_LINK") 
+
+botusername = str(getenv("BOT_USERNAME"))
+
+if str(getenv("STRING_SESSION1")).strip() == "":
+    STRING1 = str(None)
+else:
+    STRING1 = str(getenv("STRING_SESSION1"))
+
+if str(getenv("STRING_SESSION2")).strip() == "":
+    STRING2 = str(None)
+else:
+    STRING2 = str(getenv("STRING_SESSION2"))
+
+if str(getenv("STRING_SESSION3")).strip() == "":
+    STRING3 = str(None)
+else:
+    STRING3 = str(getenv("STRING_SESSION3"))
+
+if str(getenv("STRING_SESSION4")).strip() == "":
+    STRING4 = str(None)
+else:
+    STRING4 = str(getenv("STRING_SESSION4"))
+
+if str(getenv("STRING_SESSION5")).strip() == "":
+    STRING5 = str(None)
+else:
+    STRING5 = str(getenv("STRING_SESSION5"))
+
+if str(getenv("LOG_SESSION")).strip() == "":
+    LOG_SESSION = str(None)
+else:
+    LOG_SESSION = str(getenv("LOG_SESSION"))
+
+if str(getenv("LIMIT")).strip().upper() == "FALSE":
+    PL_LIMIT = "FALSE"
+else:
+    PL_LIMIT = "TRUE"
+
+if str(getenv("PM_PERMIT")).strip().upper() == "FALSE":
+    PM_PERMIT = "FALSE"
+else:
+    PM_PERMIT = "TRUE"
